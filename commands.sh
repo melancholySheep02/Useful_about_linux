9a36bce898cd00ea05848951d2a53911cca40f56
Vikrant Yadav <viki.sid19@gmail.com>
2017-01-27 11:49:42 +0530

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,7 @@
+#to remove some package 
+sudo apt-get --purge remove package
+#xprop:to find the class name of any program in linux
+
+#figlet: to cutomize the terminal e.g. adding name or something
+
+
